c5d0553f9c1eb7613c8f8824911e4ef2e46586ac
SlashTM <95855221+SlashTM@users.noreply.github.com>
2023-04-09 16:32:51 +0000

    changed .replit

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
-/* hello.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int countOccurrences(char sentence[], char letter) {
+    int count = 0;
+    for (int i = 0; i < strlen(sentence); i++) {
+        if (sentence[i] == letter) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    char sentence[1000];
+    char letter;
+    printf("Введите предложение: ");
+    fgets(sentence, 1000, stdin);
+    printf("Введите букву: ");
+    scanf("%c", &letter);
+    int occurrences = countOccurrences(sentence, letter);
+    printf("Буква '%c' встречается %d раз(а) в предложении.\n", letter, occurrences);
+    return 0;
 }
